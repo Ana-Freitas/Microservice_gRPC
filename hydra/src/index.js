e8d6c354403ad5f3dfb72aae5bbc8dc9efc4f7bf
Ana Freitas <ana_paula.10@hotmail.com>
2021-08-19 10:12:34 -0300

    First Commit: Hydra/users creating protocol buffers

---
+++ hydra/src/index.js
@@ -0,0 +1,18 @@
+const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader');
+const path = require('path');
+
+    const packageDefinition = protoLoader.loadSync(
+        path.resolve(__dirname, 'pb', 'messages.proto'),
+        {
+            keepCase: true,
+            longs: String,
+            enums: String,
+            defaults: true,
+            oneofs: true,
+        }
+    );
+
+    const proto = grpc.loadPackageDefinition(packageDefinition);
+
+    console.log(proto);
